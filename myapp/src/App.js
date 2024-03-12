efae4e148ba34202c884f264f4a2ac7cb369b4b5
Geetha <geetha.j@aifalabs.com>
2024-03-12 17:22:44 +0530

    dummy

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Apple from './apple'
+import Sample from './example/Sample';
+
+function App() {
+  return (
+    <div className="App">
+    <Sample/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/example/Sample.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Mango=()=>{
+    return(
+        <h2>mangoes are sweet</h2>
+    )
+}
+
+function Sample() {
+  return (
+    <div>
+      <Mango/>
+    </div>
+  )
+}
+
+export default Sample
